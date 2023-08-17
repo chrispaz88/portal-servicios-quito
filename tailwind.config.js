787901c044417abbddca9ae451cdbb4617e3fb6e
D4M1GOD <damian20002010@live.com>
2023-08-17 14:55:47 -0500

    V2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        '242D5F': '#242D5F',
+        '4A74A3': '#4A74A3',
+        'F6F6F6': '#F6F6F6',
+        '858282': '#858282',
+      },
+      fontFamily: {
+        'iek-web': ['iek web', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }

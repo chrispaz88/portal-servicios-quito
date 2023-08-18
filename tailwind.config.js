fdc1f5a713bdcca0a3271cad6fd6f71d0cda373c
D4M1GOD <damian20002010@live.com>
2023-08-18 13:04:38 -0500

    V3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         '4A74A3': '#4A74A3',
         'F6F6F6': '#F6F6F6',
         '858282': '#858282',
+        'F5F5F5': '#F5F5F5',
+        '0D2BD2': '#0D2BD2',
+        'E3292F': '#E3292F',
       },
       fontFamily: {
         'iek-web': ['iek web', 'sans-serif'],
